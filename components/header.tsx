d8742c22c053d1beb4baa160bf8c7c44c6801c6a
digitalartist221 <sall.aboubacre@gmail.com>
2025-01-03 18:25:32 +0100

    Package corriger next mise a jour

--- components/header.tsx
+++ components/header.tsx
@@ -1,59 +1,65 @@
+// components/Header.tsx
 import { auth, signOut } from '@/auth';
 import Link from 'next/link';
 
 const Header = async () => {
+  // Appel à `auth()` pour récupérer la session
   const session = await auth();
   const user = session?.user;
 
+  // Définir l'action pour la déconnexion
   const logoutAction = async () => {
-    'use server';
+    'use server'; // Indique que cette fonction s'exécute côté serveur
     await signOut();
   };
 
   return (
-    <header className='bg-white h-20'>
-      <nav className='h-full flex justify-between container items-center'>
+    <header className="bg-white h-20">
+      <nav className="h-full flex justify-between container items-center">
         <div>
-          <Link href='/' className='text-ct-dark-600 text-2xl font-semibold'>
+          <Link href="/" className="text-ct-dark-600 text-2xl font-semibold">
             CodevoWeb
           </Link>
         </div>
-        <ul className='flex items-center space-x-4'>
+        <ul className="flex items-center space-x-4">
           <li>
-            <Link href='/' className='text-ct-dark-600'>
+            <Link href="/" className="text-ct-dark-600">
               Home
             </Link>
           </li>
-          {!user && (
+          {!user ? (
             <>
               <li>
-                <Link href='/register' className='text-ct-dark-600'>
+                <Link href="/register" className="text-ct-dark-600">
                   Register
                 </Link>
               </li>
               <li>
-                <Link href='/login' className='text-ct-dark-600'>
+                <Link href="/login" className="text-ct-dark-600">
                   Login
                 </Link>
               </li>
             </>
-          )}
-          {user && (
-            <form action={logoutAction} className='flex'>
+          ) : (
+            <>
               <li>
-                <Link href='/client-side' className='text-ct-dark-600'>
+                <Link href="/client-side" className="text-ct-dark-600">
                   Client
                 </Link>
               </li>
-              <li className='ml-4'>
-                <Link href='/profile' className='text-ct-dark-600'>
+              <li className="ml-4">
+                <Link href="/profile" className="text-ct-dark-600">
                   Profile
                 </Link>
               </li>
-              <li className='ml-4'>
-                <button>Logout</button>
+              <li className="ml-4">
+                <form action={logoutAction}>
+                  <button type="submit" className="text-ct-dark-600">
+                    Logout
+                  </button>
+                </form>
               </li>
-            </form>
+            </>
           )}
         </ul>
       </nav>
